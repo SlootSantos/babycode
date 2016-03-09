59b5e7f99e7cbe6606995e7af4b18079a9ba7f26
Flosloot <SlootSantos@users.noreply.github.com>
2016-03-09 09:00:54 +0100

    Update main.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,6 +14,6 @@ angular.module('babycodeApp')
       window.location = '/#/f1';
     };
     $scope.no = function () {
-      alert('sag ich ja.. pussy!');
+      window.alert('sag ich ja.. pussy!');
     };
   });
