709504b74d345c9a8b2b4cc555715ca195478df2
Flosloot <SlootSantos@users.noreply.github.com>
2016-03-10 14:09:29 +0100

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,7 +68,7 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: '104.236.230.104',
         livereload: 35729
       },
       livereload: {
